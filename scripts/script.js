df96eff73d6f9a371aa7d28f22cd399be0407bb9
CarmenRozendaal <56074362+CarmenRozendaal@users.noreply.github.com>
2020-09-28 12:17:15 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,20 +2,10 @@
 var navbar = document.querySelector(".navbar")
 var ham = document.querySelector(".ham")
 
-// toggles hamburger menu in and out when clicking on the hamburger
+// toggles hamburger menu wanneer erop geklikt wordt
 function toggleHamburger(){
   navbar.classList.toggle("showNav")
   ham.classList.toggle("showClose")
 }
 
 ham.addEventListener("click", toggleHamburger)
-
-// toggle when clicking on links
-
-// METHOD 1
-var menuLinks = document.querySelectorAll(".menuLink")
-menuLinks.forEach( 
-  function(menuLink) { 
-    menuLink.addEventListener("click", toggleHamburger) 
-  }
-)
