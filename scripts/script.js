3e0484c23ab79ffe998d740dfd72ef6bc3149ef3
CarmenRozendaal <56074362+CarmenRozendaal@users.noreply.github.com>
2020-10-15 20:43:05 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -9,12 +9,25 @@
 // kruisAnimatie.addEventListener("click",AnimatieTonen);
 
 
-var navbar = document.querySelector("nav")
-var ham = document.querySelector("#hamburgermenu")
+// Hamburger menu micro interactie
+var navbar = document.querySelector("nav");
+var ham = document.querySelector(".hamburgermenu");
 
 // toggles hamburger menu wanneer erop geklikt wordt
 function toggleHamburger(){
-  navbar.classList.toggle("showNav")
+  navbar.classList.toggle("showNav");
+  ham.classList.toggle("showClose");
 }
  
-ham.addEventListener("click", toggleHamburger)
+ham.addEventListener("click", toggleHamburger);
+
+
+// Audio afspelen bij click Pringles-Bus detailpagina
+var PringlesBus = document.querySelector("div img");
+var ShakeAudio = document.querySelector('#shake');
+
+function shaken(event) {
+  ShakeAudio.play();
+}
+
+PringlesBus.addEventListener('click',shaken);
